8f990b2d4cb2bbd42202d366ab7e95a8d7dbface
aidenhu <99224270+CS2Student@users.noreply.github.com>
2023-06-11 14:26:05 -0400

    add skills collapsible

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
+const collapsible = document.getElementsByClassName("collapsible");
 
+/* Hamburger */
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -10,3 +12,17 @@ document.querySelectorAll(".media-item").forEach(n => n.addEventListener("click"
 hamburger.classList.remove("active");
 navMenu.classList.remove("active");
 }));
+
+/* Collapsible menus */
+for (let i = 0; i < collapsible.length; i++) {
+    collapsible[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        }
+        else {
+            content.style.maxHeight = content.scrollHeight  + "px";
+        }
+    });
+}
